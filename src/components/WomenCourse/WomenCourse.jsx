69712585905ce4fcc3ccac388b393cd986ac950f
Aditi Roy <122544316+imcalledaditi@users.noreply.github.com>
2025-02-04 21:44:38 +0530

    update

--- src/components/WomenCourse/WomenCourse.jsx
+++ src/components/WomenCourse/WomenCourse.jsx
@@ -24,7 +24,8 @@ const WomenCourse = () => {
     { id: 5, title: "Noel Deyzel", image: Noel, link: "https://www.youtube.com/channel/UCMp-0bU-PA7BNNR-zIvEydA" },
     { id: 6, title: "Jeff Nippard", image: Jeff, link: "https://jeffnippard.com/pages/about" },
     { id: 7, title: "Dr. Mike Israetel", image: Dr, link: "https://www.strongerexperts.com/pages/mikeisraetel" },
-    { id: 8, title: "Athlean-X", image: X, link: "https://athleanx.com/the-coach" }
+    { id: 8, title: "Athlean-X", image: X, link: "https://athleanx.com/the-coach" },
+    { id: 10, title: "Athlean-X", image: X, link: "https://athleanx.com/the-coach" }
   ];
 
   const womenCards = [
